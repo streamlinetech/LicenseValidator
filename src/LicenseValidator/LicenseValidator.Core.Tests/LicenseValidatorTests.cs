4122ff23e3ce6d475d154d10bf7b81301a4240e2
Tyler Garlick <tjgarlick@gmail.com>
2014-01-15 11:23:06 -0500

    added missing files

---
+++ src/LicenseValidator/LicenseValidator.Core.Tests/LicenseValidatorTests.cs
@@ -0,0 +1,19 @@
+﻿using FlitBit.IoC;
+using LicenseValidator.Core;
+using Xunit;
+
+namespace Validator.Core.Tests
+{
+    public class LicenseValidatorTests : AbstractTests
+    {
+        [Fact]
+        public void ValidateValidLicense()
+        {
+            using (Create.SharedOrNewContainer())
+            {
+                //var licenseValidator = Create.New<ILicenseValidator>();
+                //var response = Validate.LicenseByState(8, "USA", "GA");
+            }
+        }
+    }
+}
---
+++ src/LicenseValidator/LicenseValidator.Core/Validate.cs
@@ -0,0 +1,69 @@
+﻿using FlitBit.IoC;
+using LicenseValidator.Core.Dtos;
+
+namespace LicenseValidator.Core
+{
+    public static class Validate
+    {
+        public static ILicenseValidationResponse LicenseByState(int shipFromLocationId, string countryCode, string stateCodeOrName)
+        {
+            var request = SetupLicenseByStateRequest(shipFromLocationId, countryCode, stateCodeOrName);
+            var licenseValidator = SetupLicenseValidator();
+            var response = licenseValidator.ValidateLicenseByState(request);
+            return response;
+        }
+
+        public static ILicenseValidationResponse LicenseByState(int shipFromLocationId, string countryCode, string stateCodeOrName, string purchaseOrderApiUrl)
+        {
+            var request = SetupLicenseByStateRequest(shipFromLocationId, countryCode, stateCodeOrName);
+            var licenseValidator = SetupLicenseValidator(purchaseOrderApiUrl);
+            var response = licenseValidator.ValidateLicenseByState(request);
+            return response;
+        }
+
+        public static ILicenseValidationResponse LicenseByOrder(int shipFromLocationId, int orderId)
+        {
+            var request = SetupLicenseByOrder(shipFromLocationId, orderId);
+            var licenseValidator = SetupLicenseValidator();
+            var response = licenseValidator.ValidateLicenseByOrder(request);
+            return response;
+        }
+
+        public static ILicenseValidationResponse LicenseByOrder(int shipFromLocationId, int orderId, string purchaseOrderApiUrl)
+        {
+            var request = SetupLicenseByOrder(shipFromLocationId, orderId);
+            var licenseValidator = SetupLicenseValidator(purchaseOrderApiUrl);
+            var response = licenseValidator.ValidateLicenseByOrder(request);
+            return response;
+        }
+
+        static IValidateLicenseByState SetupLicenseByStateRequest(int shipFromLocationId, string countryCode, string stateCodeOrName)
+        {
+            var request = Create.NewInit<IValidateLicenseByState>().Init(new
+                                                                         {
+                                                                             Country = countryCode,
+                                                                             State = stateCodeOrName,
+                                                                             LocationId = shipFromLocationId
+                                                                         });
+            return request;
+        }
+
+        static IValidateLicenseByOrder SetupLicenseByOrder(int shipFromLocationId, int orderId)
+        {
+            var request = Create.NewInit<IValidateLicenseByOrder>().Init(new
+                                                                         {
+                                                                             LocationId = shipFromLocationId,
+                                                                             OrderId = orderId
+                                                                         });
+
+            return request;
+        }
+
+        static ILicenseValidator SetupLicenseValidator(string url = "")
+        {
+            if (!string.IsNullOrEmpty(url))
+                return Create.NewWithParams<ILicenseValidator>(LifespanTracking.Automatic, Param.FromValue(url));
+            return Create.New<ILicenseValidator>();
+        }
+    }
+}
