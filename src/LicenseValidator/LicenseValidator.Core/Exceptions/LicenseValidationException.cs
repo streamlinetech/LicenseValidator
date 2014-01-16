cbe3be1ed6feeb4756b1738ac519844c67558822
Tyler Garlick <tjgarlick@gmail.com>
2014-01-16 15:03:41 -0500

    added license validation exception

---
+++ src/LicenseValidator/LicenseValidator.Core/Exceptions/LicenseValidationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace LicenseValidator.Core.Exceptions
+{
+    public class LicenseValidationException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public HttpResponseMessage HttpResponse { get; set; }
+    }
+}
